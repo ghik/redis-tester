9f118cb4cee087e6d49ec65f78bfb863a05ec16a
ghik <romeqjanoosh@gmail.com>
2015-06-24 17:19:01 +0200

    initial

---
+++ src/main/java/com/avsystem/mongodb/OptimisticLocking.java
@@ -0,0 +1,8 @@
+package com.avsystem.mongodb;
+
+/**
+ * Author: ghik
+ * Created: 24/06/15.
+ */
+public class OptimisticLocking {
+}
---
+++ src/main/java/com/avsystem/mongodb/User.java
@@ -0,0 +1,14 @@
+package com.avsystem.mongodb;
+
+public class User {
+    private final String id;
+    private String name;
+    private String passwordHash;
+    private String
+
+    public User(String id) {
+        this.id = id;
+    }
+
+
+}
